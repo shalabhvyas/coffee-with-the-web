df52397f96057e0a47d0193de3cb748d4d0980e4
Shalabh Vyas <shalabhsvyas@gmail.com>
2014-02-27 17:22:57 -0800

    Adding boilerplate for user-media-api.

--- user-media-api/js/main.js
+++ user-media-api/js/main.js
@@ -1,1 +1,37 @@
+var gestureRecognizer = {};
 
+
+gestureRecognizer.hasUserMedia = function(){ 
+	return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || 
+			  navigator.mozGetUserMedia || navigator.msGetUserMedia);
+};
+
+
+gestureRecognizer.initUserMedia = function(){
+
+	var startButton = $('#startButton');
+
+	if(this.hasUserMedia()){
+
+		var localMediaStream = null;
+		var video = document.querySelector('video');
+
+		var errorCallback = function(e) {
+    			console.log('Rejected media capture!!', e);
+  		};
+
+		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || 
+			  navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+		navigator.getUserMedia({video: true, audio: false},function(stream){
+			video.src = window.URL.createObjectURL(stream);
+    		localMediaStream = stream;
+
+    		startButton.removeClass('btn-primary').addClass('btn-danger').text('Stop Capturing');
+
+		},errorCallback);
+
+	}else{
+		alert('getUserMedia() is not supported in your browser');
+	}
+};
