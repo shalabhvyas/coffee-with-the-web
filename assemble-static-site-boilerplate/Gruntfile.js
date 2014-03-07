b3e0e1eeecb06fa93f4898aac37927ccae373797
Shalabh Vyas <shalabhsvyas@gmail.com>
2014-03-07 18:04:32 +0530

    Adding Grunt tasks for watch
    Watching SCSS files, JS files and template files.

--- assemble-static-site-boilerplate/Gruntfile.js
+++ assemble-static-site-boilerplate/Gruntfile.js
@@ -27,6 +27,11 @@ module.exports = function(grunt) {
         files: {
           '<%= config.build_path %>/': ['<%= config.templates %>/*.hbs']
         }
+      },
+      design: {
+        files: {
+          './': ['<%= config.templates %>/*.hbs']
+        }
       }
     },
 
@@ -35,55 +40,88 @@ module.exports = function(grunt) {
       dist: {
         files: [{
           expand: true,
-          cwd: 'assets/scss',
+          cwd: '<%= config.assets %>/scss',
           src: ['*.scss'],
-          dest: 'assets/css',
+          dest: '<%= config.assets %>/css',
           ext: '.css'
         }]
       }
     },
 
-    // Lint JavaScript
-    jshint: {
-      all: ['Gruntfile.js', '<%= config.assets %>/js/**/*.js'],
-      options: {
-        jshintrc: '.jshintrc'
+    cssmin: {
+      combine: {
+        options:{
+          keepSpecialComments : 0
+        },
+        files: {
+          '<%= config.build_path %>/<%= config.app_name %>.min.css': ['<%= config.assets %>/css/**/*.css']
+        }
       }
     },
 
+    // Lint JavaScript
+    jshint: {
+      all: ['Gruntfile.js', '<%= config.assets %>/js/**/*.js'],      
+      options: {
+        jshintrc: '.jshintrc',
+        force: true
+      }
+    },
+
+    //Minify JS and CSS files
+    uglify: {
+      build: {
+        files: {
+          '<%= config.build_path %>/<%= config.app_name %>.min.js': ['<%= config.assets %>/js/**/*.js']          
+        }
+      }
+    },
+
+    //Clean the build folder
     clean: {
       build: ["<%= config.build_path %>"]
     },
 
+    copy: {
+      build: {
+        //Copy only images and fonts. CSS and JS are minified
+        src: ['<%= config.assets %>/img','<%= config.assets %>/fonts'],
+        dest: '<%= config.build_path %>/',
+        expand: true
+      }
+    },
+
+    //Watch the CSS and JS files.
     watch: {
       styles: {
-        files: ['<%= config.assets %>/scss/*.scss'],
-        tasks: ['sass'],
-        options: {
-          spawn: false          
-        }
+        //Watch SASS files to compile to CSS files
+        files: ['<%= config.assets %>/scss/**/*.scss'],
+        tasks: ['sass']        
       },
       scripts: {
+        //Watch JS files to do linting
         files: ['<%= config.assets %>/js/**/*.js'],
-        tasks: ['jshint'],
-        options: {
-          spawn: false          
-        }
+        tasks: ['jshint']
+      },
+      templates: {
+        //Watch template and data files
+        files: ['<%= config.templates %>/**/*.hbs','<%= config.data %>/*.json'],
+        tasks: ['assemble:design']
       }
-
     }
 
   });
 
   grunt.loadNpmTasks('assemble');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('build', ['clean','sass','jshint', 'assemble','copy']);
-  grunt.registerTask('design', ['clean','', 'sync']);
+  grunt.registerTask('build', ['clean','sass','jshint', 'assemble:build','uglify','cssmin','copy']);
+  grunt.registerTask('design', ['clean','sass','jshint', 'assemble:design', 'watch']);
 
 };
